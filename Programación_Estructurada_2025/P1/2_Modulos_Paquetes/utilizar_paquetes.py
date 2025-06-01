fa9dfe17e6775d0a3ced97f0a469642c84aad0e6
Manuel Jose <joseguerrerosimental@gmail.com>
2025-06-01 13:51:09 -0600

    primer commit

---
+++ Programación_Estructurada_2025/P1/2_Modulos_Paquetes/utilizar_paquetes.py
@@ -0,0 +1,9 @@
+from paquete1 import modulos
+
+print(modulos.saludar("Daniel Contreras Ruano"))
+
+modulos.borrarPantalla()
+nom,tel=modulos.solicitarDatos2()
+print(f"\n\t.:: Agenda Telefonica ::. \n\ttTelefono: {tel}")
+
+modulos.espereTecla()
